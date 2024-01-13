3902249920c00cd69001cafcc5090f9a9244258e
zendayat <nmapzendaya@gmail.com>
2024-01-13 18:22:58 +0300

    error handling done

--- server/controllers/residencyCtrl.js
+++ server/controllers/residencyCtrl.js
@@ -2,5 +2,16 @@ import asyncHandler from 'express-async-handler'
 import {prisma} from '../config/prismaConfig.js'
 
 export const createResidency = asyncHandler(async(req, res) => {
-    console.log("endpoint created")
+    const {title, description, price, address, country, city, facilities, image, userEmail} = req.body.data;
+
+    console.log(req.body.data)
+    try{
+
+    }
+    catch(err){
+        if(err.code === "P2002"){
+            throw  new Error("A residency with address already.")
+        }
+        throw new Error(err.message)
+    }
 })
