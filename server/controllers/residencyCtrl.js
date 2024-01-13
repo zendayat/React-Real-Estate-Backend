4d9c5877c1d653a1691b841ceb00a33ac6e56d68
zendayat <nmapzendaya@gmail.com>
2024-01-13 22:52:35 +0300

    .

--- server/controllers/residencyCtrl.js
+++ server/controllers/residencyCtrl.js
@@ -6,7 +6,11 @@ export const createResidency = asyncHandler(async(req, res) => {
 
     console.log(req.body.data)
     try{
-
+        const residency = await prisma.residency.create({
+            data: {
+                title, description, price, address, country, city, facilities, image, userEmail,
+            }
+        })
     }
     catch(err){
         if(err.code === "P2002"){
