f07e95a697fac785e8e426ffa857ae05bf1547fe
zendayat <nmapzendaya@gmail.com>
2024-01-15 13:04:56 +0300

    feat: "cancel booking" function

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -58,7 +58,41 @@ export const getAllBookings = asyncHandler(async(req, res)=>{
             select: {bookedVisits: true}
         })
         res.status(200).send(bookings)
-    }catch(errr){
+    }catch(err){
+        throw new Error(err.message);
+    }
+})
+
+
+// function to cancel booking
+
+export const cancelBooking = asyncHandler(async(req, res)=>{
+    const {email} = req.body;
+    const {id} = req.params
+    try{
+        const user = await prisma.user.findUnique({
+            where: {email},
+            select: {bookedVisits: true}
+        })
+
+        const index = user.bookedVisits.findIndex((visit)=> visit.id === id)
+
+        if(index === -1){
+            res.status(404).json({message: "Booking not found"})
+        }
+        else{
+            user.bookedVisits.splice(index, 1)
+            await prisma.user.update({
+                where: {email},
+                data: {
+                    bookedVisits: user.bookedVisits
+                }
+            })
+
+            res.send("Booking cancelled succesfully")
+        }
+    }
+    catch(err){
         throw new Error(err.message);
     }
 })
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 
 
 import express from 'express';
-import { bookVisit, createUser, getAllBookings } from '../controllers/userCtrl.js';
+import { bookVisit, cancelBooking, createUser, getAllBookings } from '../controllers/userCtrl.js';
 
 const router = express.Router();
 router.post("/register", createUser)
 router.post("/bookVisit/:id", bookVisit)
 router.post("/allBookings", getAllBookings)
+router.post("/removeBooking/:id", cancelBooking )
 
 export {router as userRoute}
